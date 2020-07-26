a3116b3ee1d0283cf98caa54e2a1abb0560a5ffe
durgasrnvs80 <durgasrnvs@gmail.com>
2020-07-26 06:03:11 -0700

    updated

--- script.js
+++ script.js
@@ -1,39 +1,96 @@
 const puzzleContainer = document.querySelector("#puzzle-container");
 
-const size = 3;
-let board = [];
-let templateBoard = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,"x"]
-];
+let board;
+
+class Board {
+    constructor() {
+        this.board = [];
+        this.winningBoard = [];
+        this.size = 3;
+    }
+
+    shuffleBoard() {
+        console.log("shuffling");
+        while( this.isWinning()) {
+            shuffle(this.board, true);
+        };
+    };
+
+    showBoard() {
+        for(let rows = 0; rows < this.size; rows++){
+            for(let cols = 0; cols < this.size; cols++){
+                this.board[rows][cols].show();
+            };
+        };
+    }
+
+    updateBoard() {
+        for(let rows = 0; rows < this.size; rows++){
+            for(let cols = 0; cols < this.size; cols++){
+                this.board[rows][cols].update(rows, cols);
+            };
+        };
+    }
+
+    isWinning() {
+        let winning = true;
+        for(let rows = 0; rows < this.size; rows++){
+            for(let cols = 0; cols < this.size; cols++){
+                if (this.board[rows][cols].value != this.winningBoard[rows][cols]) {
+                    winning = false;
+                    break;
+                };
+            };
+        };
+        return winning;
+    }
+
+    celebrate() {
+        background(color(0, 0, 50, 0.5));
+        textAlign(CENTER);
+        textSize(40);
+        fill(255);
+        stroke(0);
+        strokeWeight(2);
+        text("You won!", width/2, height/2);
+    }
+};
 
 class Piece {
     constructor(rows, cols, value) {
         this.x = cols;
         this.y = rows;
+        this.value = value;
+
+        this.maxMag = 20;
         this.xSpeed = 0;
-        this.ySpeed = 0;
+        this.ySpeed = 0;   
         this.size = 100;
-        this.value = value;    
-    };
+        colorMode(HSB);
+        this.color = color(random(0, 360), 50, 100); 
+    }
 
     show() {
-        fill(200);
-        rect(this.x, this.y, this.size, this.size);
+        if (this. value == "x")
+            return;
+        fill(this.color);
         stroke(0);
-        strokeWeight(2);
-        textSize(16);
+        let borderRadius = 5;
+        rect(this.x, this.y, this.size, this.size, borderRadius);
+        textSize(22);
+        fill(0);
         text(this.value.toString(), this.x + this.size/2, this.y + this.size/2);
     }
 
-    click() {
+    clicked() {
         if (mouseX >= this.x 
             && mouseX <= this.x + this.size
             && mouseY >= this.y
             && mouseY <= this.y + this.size){
                 console.log("Clicked", this.value);
-                this.move();
+                return true;
+        } else {
+            return false;
         };               
     }
 
@@ -41,60 +98,88 @@ class Piece {
         console.log("Moving: ",this.value);
         let rows = floor(this.y / this.size);
         let cols = floor(this.x / this.size);
-        console.log(`board[${rows}][${cols}]: ${board[rows][cols].value}`);
+        console.log(`board[${rows}][${cols}]: ${board.board[rows][cols].value}`);
+        if ( !(rows - 1 < 0) ) 
+            console.log(`top; ${board.board[rows - 1][cols].value}`);
+        if ( !(cols + 1 > board.size - 1) )
+            console.log(`right: ${board.board[rows][cols + 1].value}`);
+        if ( !(rows + 1 > board.size - 1) )
+            console.log(`bottom: ${board.board[rows + 1][cols].value}`);
+        if ( !(cols - 1 < 0) )
+            console.log(`left: ${board.board[rows][cols - 1].value}`);
         //top
         if ( !(rows - 1 < 0) ) {
-            console.log(`top; ${board[rows - 1][cols].value}`);
-            let top = board[rows - 1][cols];
+            let top = board.board[rows - 1][cols];
             if (top.value == "x") {
-                let temp = board[rows][cols];
-                board[rows][cols] = top;
-                board[rows - 1][cols] = temp;
-                showBoard();
+                let temp = board.board[rows][cols];
+                board.board[rows][cols] = top;
+                board.board[rows - 1][cols] = temp;
             };
         };
         //right
-        if ( !(cols + 1 > size - 1) ) {
-            console.log(`right: ${board[rows][cols + 1].value}`);
-            let right = board[rows][cols + 1];
+        if ( !(cols + 1 > board.size - 1) ) {
+            let right = board.board[rows][cols + 1];
             if (right.value == "x") {
-                let temp = board[rows][cols];
-                board[rows][cols] = right;
-                board[rows][cols + 1] = temp;
-                showBoard();
+                let temp = board.board[rows][cols];
+                board.board[rows][cols] = right;
+                board.board[rows][cols + 1] = temp;
             };
         };
         //bottom
-        if ( !(rows + 1 > size - 1) ) {
-            console.log(`bottom: ${board[rows + 1][cols].value}`);
-            let bottom = board[rows + 1][cols];
+        if ( !(rows + 1 > board.size - 1) ) {
+            let bottom = board.board[rows + 1][cols];
             if (bottom.value == "x") {
-                let temp = board[rows][cols];
-                board[rows][cols] = bottom;
-                board[rows + 1][cols] = temp;
-                showBoard();
+                let temp = board.board[rows][cols];
+                board.board[rows][cols] = bottom;
+                board.board[rows + 1][cols] = temp;
             };
         };
         //left
         if ( !(cols - 1 < 0) ) {
-            console.log(`left: ${board[rows][cols - 1].value}`);
-            let left = board[rows][cols - 1];
+            let left = board.board[rows][cols - 1];
             if (left.value == "x") {
-                let temp = board[rows][cols];
-                board[rows][cols] = left;
-                board[rows][cols - 1] = temp;
-                showBoard();
+                let temp = board.board[rows][cols];
+                board.board[rows][cols] = left;
+                board.board[rows][cols - 1] = temp;
             };
         };
-        console.log(board);
-    };
+        console.log(board.board);
+    }
 
-    update() {
-        this.xSpeed = random(-3, 3);
-        this.ySpeed = random(-3, 3);
+    update(rows, cols) {
+        let targetX = cols * this.size;
+        let targetY = rows * this.size;
+        if (this.x != targetX) {
+            if (this.x <= targetX) {
+                this.xSpeed = this.maxMag ;
+            } else {
+                this.xSpeed = -this.maxMag ;
+            };
+        };
+        if (this.y != targetY) {
+            if (this.y <= targetY) {
+                this.ySpeed = this.maxMag ;
+            } else {
+                this.ySpeed = -this.maxMag ;
+            };
+        };
+        if (this.x == targetX && this.y == targetY) {
+            this.xSpeed = 0;
+            this.ySpeed = 0;
+        };
         this.x += this.xSpeed;
         this.y += this.ySpeed;
-    };
+    }
+}
+
+function mouseClicked() {
+    for(let rows = 0; rows < board.size; rows++){
+        for(let cols = 0; cols < board.size; cols++){
+            if (board.board[rows][cols].clicked()) {
+                board.board[rows][cols].move();
+            }
+        };
+    };    
 }
 
 function setup() {
@@ -102,56 +187,41 @@ function setup() {
     let height = 300;
     let canvas = createCanvas(width, height);
     canvas.parent("puzzle-container");
-    
-    for(let rows = 0; rows < size; rows++) {
-        board[rows] = new Array(size);
+
+    board = new Board();
+    for(let rows = 0; rows < board.size; rows++) {
+        board.board[rows] = new Array(board.size);
     };
-    for(let rows = 0; rows < size; rows++){
-        for(let cols = 0; cols < size; cols++){
+    for(let rows = 0; rows < board.size; rows++){
+        for(let cols = 0; cols < board.size; cols++){
             let spacing = 100;
-            let piece = new Piece(rows * spacing, cols * spacing, templateBoard[rows][cols]);
-            board[rows][cols] = piece;
+            let piece = new Piece(rows * spacing, cols * spacing, rows * board.size + cols);
+            board.board[rows][cols] = piece + 1;
         };
     };
-    console.log(board);
+    board.board[board.size - 1][board.size - 1].value = "x";
+
+    for(let rows = 0; rows < board.size; rows++) {
+        board.winningBoard[rows] = new Array(board.size);
+    };
+    for(let rows = 0; rows < board.size; rows++){
+        for(let cols = 0; cols < board.size; cols++){
+            board.winningBoard[rows][cols] = board.board[rows][cols].value;
+        };
+    };
+
+    console.log("Board:", board.board);
+    board.shuffleBoard();
+    console.log("Shuffled board", board.board);
+    console.log("Winning board:", board.winningBoard);
 }
 
 function draw() {
+    frameRate(60);
     background(200);
-    showBoard();
-    updateBoard();    
-    frameRate(30);
+    board.showBoard();
+    board.updateBoard();
+    if (board.isWinning() == true) {
+        board.celebrate();
+    };
 }
-
-
-function showBoard() {
-    for(let rows = 0; rows < size; rows++){
-        for(let cols = 0; cols < size; cols++){
-            let rectWidth = 100;
-            let x = cols * rectWidth;
-            let y = rows * rectWidth;
-            fill(200);
-            rect(x, y, rectWidth, rectWidth);
-            stroke(0);
-            strokeWeight(2);
-            textSize(16);
-            text(board[rows][cols].value.toString(), x + rectWidth/2, y + rectWidth/2);
-        };
-    };
-};
-
-function updateBoard() {
-    for(let rows = 0; rows < size; rows++){
-        for(let cols = 0; cols < size; cols++){
-            board[rows][cols].update();
-        };
-    };
-};
-
-function mouseClicked() {
-    for(let rows = 0; rows < size; rows++){
-        for(let cols = 0; cols < size; cols++){
-            board[rows][cols].click();
-        };
-    };    
-};
